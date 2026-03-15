640680dd0b77867c1f7854f78a244df3550029a5
Polina Spirina <polly_sp@mail.ru>
2026-03-15 15:44:07 +0300

    timer

--- dist/script.js
+++ dist/script.js
@@ -113,7 +113,6 @@ document.querySelectorAll('input[name="presence"]').forEach((radio) => {
   });
 });
 
-
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
   const allergyError = document.getElementById("allergyError");
@@ -161,3 +160,35 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+function startCountdown(targetDate) {
+  function updateTimer() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    if (distance < 0) {
+      document.getElementById("timer").innerHTML = "Мы стали семьей!";
+      document.getElementsByClassName("datetime").display
+      return;
+    }
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }
+
+  updateTimer();
+  setInterval(updateTimer, 1000);
+}
+
+// Использование: 31 декабря 2024 года
+const newYear = new Date(2026, 7, 14, 15, 0, 0).getTime();
+startCountdown(newYear);

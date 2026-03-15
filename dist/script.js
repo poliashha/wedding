e3005896f2ae6ee9451105a76b9fdbbbf6ff053a
Polina Spirina <polly_sp@mail.ru>
2026-03-15 19:49:27 +0300

    fix bag name

--- dist/script.js
+++ dist/script.js
@@ -198,9 +198,8 @@ const name = urlParams.get("name");
 
 if (name) {
   document.getElementById("greeting").textContent = `Уважаемые ${name},`;
-  document.getElementById("greeting").fontSize = "25px";
-  document.getElementById("greeting_wedding").fontSize = "25px";
-
+  document.getElementById("greeting").style.fontSize = "25px";
+  document.getElementById("greeting_wedding").style.fontSize = "25px";
 }
 else{
   document.getElementById("greeting").style.display = "none";

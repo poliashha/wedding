83e20ebc1beb5e36a3901698f4eee59c157fa683
Polina Spirina <polly_sp@mail.ru>
2026-03-18 15:48:13 +0300

    mail

--- dist/script.js
+++ dist/script.js
@@ -1,147 +1,70 @@
-// const TELEGRAM_BOT_TOKEN =
-//   "vk1.a.CRkLLm7gNQ361edpy3o7Yc8HOzUayiZ-qwrZwoiOIq7ayicDEAUt2CIE7vMuLeNN7sqEfmT6J6GjO6LMQe5TcMCpSmtVs31Xtb1-zSMrD9oO7GP15CiR4TvcC16jyCSpPQf2_rABER5U990Wi016lxAlYDOkwTUZqDIftYN8jC0xukGaHW_cMmjkNea_l0Dyb_NNYy6rf_Y2ZVQxyRztzw";
-// const TELEGRAM_CHAT_ID = "224996524";
-// const API = `https://api.vk.com/method/messages.send`;
+const TELEGRAM_BOT_TOKEN =
+  "vk1.a.CRkLLm7gNQ361edpy3o7Yc8HOzUayiZ-qwrZwoiOIq7ayicDEAUt2CIE7vMuLeNN7sqEfmT6J6GjO6LMQe5TcMCpSmtVs31Xtb1-zSMrD9oO7GP15CiR4TvcC16jyCSpPQf2_rABER5U990Wi016lxAlYDOkwTUZqDIftYN8jC0xukGaHW_cMmjkNea_l0Dyb_NNYy6rf_Y2ZVQxyRztzw";
+const TELEGRAM_CHAT_ID = "224996524";
+const API = `https://api.vk.com/method/messages.send`;
 
-// // const TELEGRAM_BOT_TOKEN = "8798342879:AAFawWVHQ4lRPqVqjYQ6X5iclJd8u-B_d_o";
-// // const TELEGRAM_CHAT_ID = "@WeddingDorogovi";
-// // const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
-
-// async function sendQuestionnaire(event) {
-//   event.preventDefault();
-//   const form = event.target;
-//   const formBth = document.querySelector(".button");
-//   const formSendResult = document.querySelector(".form-send");
-//   formSendResult.textContent = "";
-
-//   const formData = new FormData(form);
-
-//   const name = formData.get("name");
-//   const presence = formData.get("presence");
-//   const allergy = formData.get("allergy");
-//   const listallergy = formData.get("listallergy");
-//   const drinks = formData.getAll("drinks");
-
-//   const text = `Гость ${name},\nбудет присутствовать: ${presence},\nесть аллергия: ${allergy}${allergy === "да" ? `,\nна что аллергия: ${listallergy}` : ""},\nнапитки: ${drinks.join(", ")}`;
-
-//   try {
-//     formBth.textContent = "Отправка...";
-//     const response = await fetch(API, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/x-www-form-urlencoded",
-//       },
-//       body: new URLSearchParams({
-//         user_id: TELEGRAM_CHAT_ID,
-//         random_id: Date.now(),
-//         message: text,
-//         access_token: TELEGRAM_BOT_TOKEN,
-//         v: "5.131",
-//       }),
-//     });
-
-//     // const response = await fetch(API, {
-//     //   method: "POST",
-//     //   headers: {
-//     //     "Content-Type": "application/json",
-//     //   },
-//     //   body: JSON.stringify({
-//     //     chat_id: TELEGRAM_CHAT_ID,
-//     //     text,
-//     //   }),
-//     // });
-
-//     if (response.ok) {
-//       formSendResult.textContent = "Спасибо! Анкета отправлена.";
-
-//       form.reset();
-//     } else {
-//       throw new Error(response.statusText);
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     formSendResult.textContent = "Спасибо! Анкета отправлена.";
-//   } finally {
-//     formBth.textContent = "Подтверидить присутсвие";
-//   }
-// }
+// const TELEGRAM_BOT_TOKEN = "8798342879:AAFawWVHQ4lRPqVqjYQ6X5iclJd8u-B_d_o";
+// const TELEGRAM_CHAT_ID = "@WeddingDorogovi";
+// const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
 
 async function sendQuestionnaire(event) {
   event.preventDefault();
   const form = event.target;
-  const submitBtn = document.querySelector(".button");
+  const formBth = document.querySelector(".button");
   const formSendResult = document.querySelector(".form-send");
   formSendResult.textContent = "";
 
-   const originalText = submitBtn.textContent;
+  const formData = new FormData(form);
 
-   // Получаем данные из формы
-   const name = form.querySelector('[name="name"]')?.value || "";
-   const presence =
-     form.querySelector('[name="presence"]:checked')?.value || "";
-   const allergy = form.querySelector('[name="allergy"]:checked')?.value || "";
+  const name = formData.get("name");
+  const presence = formData.get("presence");
+  const allergy = formData.get("allergy");
+  const listallergy = formData.get("listallergy");
+  const drinks = formData.getAll("drinks");
 
-   // Собираем аллергии (чекбоксы)
-   const allergyItems = form.querySelectorAll(
-     'input[name="allergy_items"]:checked',
-   );
-   const listallergy = Array.from(allergyItems)
-     .map((cb) => cb.value)
-     .join(", ");
+  const text = `Гость ${name},\nбудет присутствовать: ${presence},\nесть аллергия: ${allergy}${allergy === "да" ? `,\nна что аллергия: ${listallergy}` : ""},\nнапитки: ${drinks.join(", ")}`;
 
-   // Собираем напитки
-   const drinksItems = form.querySelectorAll('input[name="drinks"]:checked');
-   const drinks = Array.from(drinksItems).map((cb) => cb.value);
+  try {
+    formBth.textContent = "Отправка...";
+    const response = await fetch(API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        user_id: TELEGRAM_CHAT_ID,
+        random_id: Date.now(),
+        message: text,
+        access_token: TELEGRAM_BOT_TOKEN,
+        v: "5.131",
+      }),
+    });
 
-   // Формируем текст сообщения
-   const text = `Гость ${name},
-будет присутствовать: ${presence},
-есть аллергия: ${allergy}${
-     allergy === "да"
-       ? `,
-на что аллергия: ${listallergy}`
-       : ""
-   },
-напитки: ${drinks.join(", ")}`;
+    // const response = await fetch(API, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     chat_id: TELEGRAM_CHAT_ID,
+    //     text,
+    //   }),
+    // });
 
-   // Создаём FormData для отправки
-   const formData = new FormData();
-   formData.append("access_key", "6fa52a1b-3311-4436-9a98-5dcf7f6db9c3");
-   formData.append("subject", "Новая анкета со свадьбы");
-   formData.append("message", text); // 👈 отправляем отформатированный текст
+    if (response.ok) {
+      formSendResult.textContent = "Спасибо! Анкета отправлена.";
 
-   // Опционально: можно добавить отдельные поля
-   
-
-
-   submitBtn.textContent = "Отправляется...";
-   submitBtn.disabled = true;
-
-   try {
-     const response = await fetch("https://api.web3forms.com/submit", {
-       method: "POST",
-       body: formData,
-     });
-
-     const data = await response.json();
-
-     if (response.ok) {
-       formSendResult.textContent = "Анкета отправлена!";
-       formSendResult.style.color = "green";
-       form.reset();
-     } else {
-       throw new Error(data.message);
-     }
-   } catch (error) {
-     console.error(error);
-     formSendResult.textContent = "Ошибка отправки";
-     formSendResult.style.color = "red";
-   } finally {
-     submitBtn.textContent = originalText; // 
-     submitBtn.disabled = false;
-   }
+      form.reset();
+    } else {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.error(error);
+    formSendResult.textContent = "Спасибо! Анкета отправлена.";
+  } finally {
+    formBth.textContent = "Подтверидить присутсвие";
+  }
 }
-
 document.addEventListener("DOMContentLoaded", function () {
   const yesRadio = document.getElementById("allergyYes");
   const noRadio = document.getElementById("allergyNo");
@@ -302,17 +225,20 @@ function initScrollAnimation() {
   const containers = document.querySelectorAll(".conteiner");
 
   // Создаём наблюдатель
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      // Если элемент появился в зоне видимости
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        // Если элемент появился в зоне видимости
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
 
-        // Останавливаем наблюдение для этого элемента
-        observer.unobserve(entry.target);
-      }
-    });
-  });
+          // Останавливаем наблюдение для этого элемента
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+   
+  );
 
   // Начинаем следить за каждым контейнером
   containers.forEach((container) => {
